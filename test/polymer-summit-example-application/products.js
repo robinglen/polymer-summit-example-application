839b2f57734e06289140c05ebd917724397f6b33
Robin Glen <robin.glen@net-a-porter.com>
2016-09-16 11:46:48 +0100

    adding some more tests

---
+++ test/polymer-summit-example-application/products.js
@@ -0,0 +1,66 @@
+var products = {
+  "products" : [
+    {
+      "locale": "en-GB",
+      "name":"Poppy off-the-shoulder embroidered cotton-poplin dress",
+      "key": "poppy-off-the-shoulder-embroidered-cotton-poplin-dress",
+      "price":{
+        "currency":"GBP",
+        "amount":2545
+      },
+      "brand":{
+        "name":"loveshackfancy",
+        "key":"loveshackfancy"
+      },
+      "id":594952,
+      "image": {
+        "primary" : "//cache.net-a-porter.com/images/products/756326/756326_in_sl.jpg",
+        "secondary" : "//cache.net-a-porter.com/images/products/756326/756326_ou_sl.jpg"
+      }
+    },
+    {
+      "locale": "en-GB",
+      "name":"Velvet-trimmed polka-dot cotton",
+      "key": "velvet-trimmed-polka-dot-cotton-and-wool-blend-blazer",
+      "price":{
+        "currency":"GBP",
+        "amount":1100
+      },
+      "brand":{
+        "name":"GUCCI",
+        "key":"gucci"
+      },
+      "id":714595,
+      "image": {
+        "primary" : "//cache.net-a-porter.com/images/products/714595/714595_in_sl.jpg",
+        "secondary" : "//cache.net-a-porter.com/images/products/714595/714595_ou_sl.jpg"
+      }
+    },
+    {
+      "locale": "en-GB",
+      "name":"Le Crop bootcut jeans",
+      "key": "le-crop-bootcut-jeans",
+      "price":{
+        "currency":"GBP",
+        "amount":225
+      },
+      "brand":{
+        "name":"FRAME",
+        "key":"Frame"
+      },
+      "id":716463,
+      "image": {
+        "primary" : "//cache.net-a-porter.com/images/products/716463/716463_in_sl.jpg",
+        "secondary" : "//cache.net-a-porter.com/images/products/716463/716463_ou_sl.jpg"
+      }
+    }
+  ],
+  "metadata": {
+    "title": "demo list"
+  },
+  "listInfo": {
+    "limit": 3,
+    "offset": 0,
+    "total": 9
+  }
+};
